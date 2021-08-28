df4da469908c49f583de003fa33ff98dc87e196f
Jordan Chappell <jordan.chappell33@gmail.com>
2021-08-28 15:01:28 +1000

    initial project commit
    - Includes ParcelJS, ReactJS, project setup, readme changes.

---
+++ codebase/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => (
+  <div className="app">
+    <header>
+      <h1 className="heading">React App Template</h1>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </header>
+  </div>
+);
+
+
+export default App;
